53e9452df3858efb07862b6bbdde2ee8ef88da76
Daniel Adams <DanielAdams227@gmail.com>
2023-10-18 17:53:12 +0100

    correct

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import HeroImage from '../asset/herolmage.jpg';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const home = () => {
     return (
-        <div name='Home' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-800'>
+        <div name='Home' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-800 '>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
 
-                <div className='flex flex-col justify-center h-full '>
-                    
+                <div className='flex flex-col justify-center h-full pt-24'>
+
                     <h2 className='text-white text-4xl sm:text-7xl font-bold'>
-                        I'm a Front End React Developer  
+                        I'm a Front End React Developer
                     </h2>
                     <p className='text-gray-500 py-4 max-w-md'>
                         I have 3 years of experience building and designing software.
@@ -20,13 +20,13 @@ const home = () => {
                     </p>
                     <div>
 
-                        
+
                         <Link to='portfolio' smooth duration={500} className='text-white w-fit px-6 py-3 my-2 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                             Portfolio
 
 
-                        </Link> 
-                    
+                        </Link>
+
                     </div>
                 </div>
 
